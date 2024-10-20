4093950c65727582d6ed39ed4279b6ae392352ba
Christine <84812987+phase5lhcm@users.noreply.github.com>
2024-10-20 18:55:33 -0400

    Make Product names be unique

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE IF NOT EXISTS Categories (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
     description TEXT
 );
 
