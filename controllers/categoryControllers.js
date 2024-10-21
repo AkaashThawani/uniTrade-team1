06079c23575d6aeb5cbfa0d486ba9450a756c502
Christine <84812987+phase5lhcm@users.noreply.github.com>
2024-10-21 02:49:24 -0400

    Add endpoints for soft delete and updating category name

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -29,9 +29,10 @@ const createCategory = async (req, res) => {
 const category = async (req, res) => {
   try {
     const { name } = req.params;
-    const [rows] = await db.query("SELECT * FROM Categories WHERE name = ?", [
-      name,
-    ]);
+    const [rows] = await db.query(
+      "SELECT * FROM Categories WHERE name = ? AND isDeleted = FALSE", // prevents soft deleted categories from being returned
+      [name]
+    );
     if (rows.length == 0) {
       return res.status(404).json({
         error: "Category does not exist.",
@@ -48,10 +49,13 @@ const category = async (req, res) => {
   }
 };
 
-const softDelete = async (req, res) => {
+const deactivateCategory = async (req, res) => {
   try {
+    console.log(req);
+    const { id } = req.body.id;
     const [result] = await db.query(
-      "UPDATE Categories SET isDeleted = TRUE... WHERE category_id = ?"[id]
+      "UPDATE Categories SET isDeleted = TRUE WHERE id = ?",
+      [id]
     );
     if (result.affectedRows.length == 0) {
       return res.status(404).json({
@@ -59,14 +63,48 @@ const softDelete = async (req, res) => {
       });
     }
     res.status(200).json({
-      message: "Category marked for deletion",
+      message: "Category marked for deactivation",
     });
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      error: "An error occurred while attempting to delete category.",
+      error: "An error occurred while attempting to deactivate category.",
     });
   }
 };
 
-module.exports = { category, createCategory, softDelete };
+const update = async (req, res) => {
+  try {
+    const { id, name } = req.body;
+
+    //first, we check if the updated name already exists
+    const [categoryExists] = await db.query(
+      "SELECT category WHERE name = ?"[(name, id)]
+    );
+
+    if (categoryExists && categoryExists.length > 0) {
+      return res.status(400).json({
+        error: "Category with this name already exists.",
+      });
+    }
+
+    const [result] = await db.query(
+      "UPDATE Categories SET name = ? WHERE category_id = ?"[(name, id)]
+    );
+    if (result.affectedRows.length == 0) {
+      return res.status(404).json({
+        error: "Category not found.",
+      });
+    }
+    res.status(200).json({
+      message: "Category updated successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "An error occurred while attempting to update the category.",
+    });
+  }
+};
+
+module.exports = { category, createCategory, deactivateCategory, update };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,12 +3,15 @@ const {
   createCategory,
   category,
   softDelete,
+  update,
+  deactivateCategory,
 } = require("../controllers/categoryControllers");
 
 const router = express.Router();
 
 router.post("/categories", createCategory);
 router.get("/category/:name", category);
-router.put("/delete/:id", softDelete);
+router.put("/delete/:category_id", deactivateCategory);
+router.put("/category/:id", update);
 
 module.exports = router;
