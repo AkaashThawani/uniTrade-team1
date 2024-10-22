ebcac37202449da8ea73d1f95a77092682526204
Christine <84812987+phase5lhcm@users.noreply.github.com>
2024-10-21 22:18:48 -0400

    Add endpoints for Products

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,16 +2,15 @@ const express = require("express");
 const {
   createCategory,
   category,
-  softDelete,
   update,
   deactivateCategory,
-} = require("../controllers/categoryControllers");
+} = require("../controllers/CategoryControllers");
 
 const router = express.Router();
 
 router.post("/categories", createCategory);
 router.get("/category/:name", category);
-router.put("/delete/:category_id", deactivateCategory);
+router.put("/deactivateCategory/:category_id", deactivateCategory);
 router.put("/category/:id", update);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 //const routes = require("./routes/index");
-const categoryRoutes = require("./routes/categoryRoutes")
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
 const config = require("./config.json");
 const middlewares = require("./middlewares/index");
 
@@ -13,10 +14,10 @@ app.use(express.json());
 // setup routes
 //routes(app);
 
-app.use('/api', categoryRoutes)
-
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 const PORT = 3000;
 // start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
