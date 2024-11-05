5ae5d993e09215131046193cee74fc3efb598739
Akaash Thawani <akaashthawani123@gmail.com>
2024-11-04 20:32:32 -0500

    corrected import

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const {
-  product,
+  deleteProduct,
   createProduct,
   updateProduct,
+  getProductByName,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.post("/products", createProduct);
-router.get("/product/:name", product);
+router.get("/product/:name", getProductByName);
 router.put("/product/:id", updateProduct);
+router.post("/deleteProduct", deleteProduct);
 
 module.exports = router;
