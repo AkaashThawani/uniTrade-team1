c3a87d64bbc387016b5152de163ba175a67af91b
Akaash Thawani <akaashthawani123@gmail.com>
2024-12-07 11:15:30 -0500

    remove wallet api

--- server.js
+++ server.js
@@ -23,21 +23,6 @@ app.use("/api", marketDataRoutes);
 const PORT = 3000;
 
 
-app.get('/api/wallet/:user_id', async (req, res) => {
-  const userId = req.params.user_id;
-  try {
-      const result = await database.query('SELECT user_id, available_balance FROM public.member_wallet WHERE user_id = $1', [userId]);
-      if (result.rows.length === 0) {
-          return res.status(404).json({ error: "User not found" });
-      }
-      res.json(result.rows[0]);
-  } catch (error) {
-      console.error("Database query error:", error);
-      res.status(500).json({ error: "Database query error" });
-  }
-});
-
-
 // start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
