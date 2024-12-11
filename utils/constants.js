d8c80a5d1ae7585478ddc671bda8461a2e7d7f4c
Akaash Thawani <akaashthawani123@gmail.com>
2024-12-11 16:42:32 -0500

    added middleware

---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+const constants = {};
+
+constants.whiteListAPIs = [
+    "/auth/register",
+    "/auth/login",
+    "/auth/session",
+    "/api/recentCategories",
+    "recentCategories"
+]
+
+module.exports = constants;
